24dd638866ed9678780049273241108fa0487517
João Senna Radicchi <joaoradicchi@gmail.com>
2023-06-20 13:41:23 -0600

    Implement Fligh class

--- TravelessApp/Data/Flight.cs
+++ TravelessApp/Data/Flight.cs
@@ -2,5 +2,24 @@
 
 public class Flight
 {
-    
+    public string FlightCode { get; set; }
+    public string Airline { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Day { get; set; }
+    public string Time { get; set; }
+    public int AvailableSeats { get; set; }
+    public double Cost { get; set; }
+
+    public Flight(string flightCode, string airline, string from, string to, string day, string time, int availableSeats, double cost)
+    {
+        FlightCode = flightCode;
+        Airline = airline;
+        From = from;
+        To = to;
+        Day = day;
+        Time = time;
+        AvailableSeats = availableSeats;
+        Cost = cost;
+    }
 }
