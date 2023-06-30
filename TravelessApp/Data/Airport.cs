4ad94b9f8030a14cffb0d0cf806d730fa7350b2a
João Senna Radicchi <joaoradicchi@gmail.com>
2023-06-29 19:22:17 -0600

    Add Airport class

---
+++ TravelessApp/Data/Airport.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelessApp.Data;
+
+public class Airport
+{
+    public string Code { get; }
+    
+    public string FullName { get; }
+
+    public Airport(string code, string fullName)
+    {
+        Code = code;
+        FullName = fullName;
+    }
+}
